de2ca79280bd262c1e5328af94fbfff05fc556c2
Taneli Järvenpää <114722920+TaneliJarvenpaa@users.noreply.github.com>
2023-09-13 11:02:29 +0300

    Add files via upload
    added Rest services, added Rest booklisting and Rest finding book by id, modified book class and added Spring Rest dependencies and new api line to application properties

--- Bookstore/Bookstore/Bookstore/BookController.java
+++ Bookstore/Bookstore/Bookstore/BookController.java
@@ -1,11 +1,15 @@
 package bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import bookstore.domain.Book;
 import bookstore.domain.BookRepository;
@@ -19,6 +23,18 @@ public class BookController {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	//Rest service get all the book to JSON-format
+	@GetMapping("/bookrest")
+	public @ResponseBody List <Book> bookListRest(){
+		return (List<Book>) repository.findAll();
+	}
+	
+	//Rest service to get book by id
+	@GetMapping("/bookrest/{id}")
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id")Long bookId){
+		return repository.findById(bookId);
+	}
 
 	@GetMapping("booklist")
 	public String bookList(Model model) {
